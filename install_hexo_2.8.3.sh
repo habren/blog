697532a157ed3211200a6a56986d87f2fb5cfc43
Jason Guo <jason.guo.vip@gmail.com>
2015-12-27 14:20:33 +0800

    update hexo 2.8.3 installation script

--- install_hexo_2.8.3.sh
+++ install_hexo_2.8.3.sh
@@ -1,1 +1,3 @@
-npm install hexo@2.8.3 hexo-renderer-ejs@0.1.0 hexo-renderer-marked@0.1.0 hexo-renderer-stylus@0.1.0 hexo-generator-feed@0.2.1 hexo-generator-sitemap@0.2.0 -g 
+npm install hexo@2.8.3 hexo-renderer-ejs@0.1.0 hexo-renderer-marked@0.1.0 hexo-renderer-stylus@0.1.0 hexo-generator-feed@0.2.1 hexo-generator-sitemap@0.2.0 -g
+npm install hexo@2.8.3 hexo-renderer-ejs@0.1.0 hexo-renderer-marked@0.1.0 hexo-renderer-stylus@0.1.0 hexo-generator-feed@0.2.1 hexo-generator-sitemap@0.2.0 --save 
+ 
